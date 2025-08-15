6700afbb5fcb54fd387b74ce8a33db6d52e6d7ce
qnjl18 <queenanjelmaghinay8@gmail.com>
2025-08-15 15:23:48 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let count = 0;
+const button = document.getElementById("counterBtn");
+button.addEventListener("click", () => {
+    count++;
+    button.textContent = `You clicked me ${count} times`;
+});
